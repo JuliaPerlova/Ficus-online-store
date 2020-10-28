7e2b2e283b96a0ba2d11fffb9e502ee5fc2b33db
Vladyslav Fylymonov <fylymonov.vladyslav@gmail.com>
2020-10-28 10:40:38 +0200

    add signin and signup pages

--- frontend/src/pages/SignIn/SignIn.js
+++ frontend/src/pages/SignIn/SignIn.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
 
 export const SignIn = () => {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-      <h1>SignIn page</h1>
-    </div>
+    <>
+      <h2>Welcome</h2>
+      <Form name='signin' className='form' size='large' onFinish={onFinish}>
+        <Form.Item
+          name='email'
+          rules={[{ required: true, message: "Please fill the field" }]}
+        >
+          <Input prefix={<MailOutlined />} placeholder='Email' />
+        </Form.Item>
+        <Form.Item
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input prefix={<LockOutlined />} placeholder='Password' />
+        </Form.Item>
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Sign in
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -1,130 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.css";
 import { Form, Input, Button, Upload } from "antd";
-import {
-  UserOutlined,
-  LockOutlined,
-  MailOutlined,
-  InboxOutlined,
-} from "@ant-design/icons";
+import ImgCrop from "antd-img-crop";
+import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 
 export const SignUp = () => {
   const onFinish = (values) => {
     console.log(values);
   };
 
-  const normFile = (e) => {
-    console.log("Upload event:", e);
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e && e.fileList;
-  };
-
   return (
-    <Form name='signup' className='form' onFinish={onFinish}>
-      <Form.Item
-        name='email'
-        rules={[{ required: true, message: "Please fill the field" }]}
-      >
-        <Input prefix={<MailOutlined />} placeholder='Email' />
-      </Form.Item>
-      <Form.Item
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: "Please input your password!",
-          },
-        ]}
-        hasFeedback
-      >
-        <Input prefix={<LockOutlined />} placeholder='Password' />
-      </Form.Item>
-
-      <Form.Item
-        name='confirm'
-        dependencies={["password"]}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: "Please confirm your password!",
-          },
-          ({ getFieldValue }) => ({
-            validator(rule, value) {
-              if (!value || getFieldValue("password") === value) {
-                return Promise.resolve();
-              }
-
-              return Promise.reject(
-                "The two passwords that you entered do not match!"
-              );
+    <>
+      <h2>Registration form</h2>
+      <Form name='signup' className='form' size='large' onFinish={onFinish}>
+        <Form.Item
+          name='email'
+          rules={[{ required: true, message: "Please fill the field" }]}
+        >
+          <Input prefix={<MailOutlined />} placeholder='Email' />
+        </Form.Item>
+        <Form.Item
+          name='username'
+          rules={[{ required: true, message: "Please fill the field" }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder='Username' />
+        </Form.Item>
+        <Form.Item
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
             },
-          }),
-        ]}
-      >
-        <Input prefix={<LockOutlined />} placeholder='Confirm password' />
-      </Form.Item>
+          ]}
+          hasFeedback
+        >
+          <Input prefix={<LockOutlined />} placeholder='Password' />
+        </Form.Item>
+        <Form.Item
+          name='confirm'
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(rule, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
 
-      <Form.Item
-        name='dragger'
-        valuePropName='fileList'
-        getValueFromEvent={normFile}
-        noStyle
-      >
-        <Upload.Dragger name='files' action='/upload.do'>
-          <p className='ant-upload-drag-icon'>
-            <InboxOutlined />
-          </p>
-          <p className='ant-upload-text'>
-            Click or drag your avatar to this area to upload
-          </p>
-          <p className='ant-upload-hint'>
-            Support for a single or bulk upload.
-          </p>
-        </Upload.Dragger>
-      </Form.Item>
-    </Form>
+                return Promise.reject(
+                  "The two passwords that you entered do not match!"
+                );
+              },
+            }),
+          ]}
+        >
+          <Input prefix={<LockOutlined />} placeholder='Confirm password' />
+        </Form.Item>
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
-
-{
-  /* <Form.Item
-name="password"
-label="Password"
-rules={[
-  {
-    required: true,
-    message: 'Please input your password!',
-  },
-]}
-hasFeedback
->
-<Input.Password />
-</Form.Item>
-
-<Form.Item
-name="confirm"
-label="Confirm Password"
-dependencies={['password']}
-hasFeedback
-rules={[
-  {
-    required: true,
-    message: 'Please confirm your password!',
-  },
-  ({ getFieldValue }) => ({
-    validator(rule, value) {
-      if (!value || getFieldValue('password') === value) {
-        return Promise.resolve();
-      }
-
-      return Promise.reject('The two passwords that you entered do not match!');
-    },
-  }),
-]}
->
-<Input.Password />
-</Form.Item> */
-}
